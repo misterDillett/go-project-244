534ca6f94037e767317618701ac485514c21a96c
Varava Maxim <mister.dilletttt@hotmail.com>
2026-03-08 21:56:36 +0300

    add sonar-project.properties

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/misterDillett/go-project-244/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/misterDillett/go-project-244/actions)
+
+# Gendiff
+
+[![CI](https://github.com/misterDillett/go-project-244/actions/workflows/ci.yml/badge.svg)](https://github.com/misterDillett/go-project-244/actions/workflows/ci.yml)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=go-project-244&metric=coverage)](https://sonarcloud.io/summary/new_code?id=go-project-244)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=go-project-244&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=go-project-244)
--- differ_test.go
+++ differ_test.go
@@ -1,21 +1,21 @@
 package code
 
 import (
-	"testing"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
 )
 
 func TestGenDiff_FlatJSON(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1.json",
-		"testdata/fixture/file2.json",
-		"stylish",
-	)
-	require.NoError(t, err)
+    result, err := GenDiff(
+        "testdata/fixture/file1.json",
+        "testdata/fixture/file2.json",
+        "stylish",
+    )
+    require.NoError(t, err)
 
-	expected := `{
+    expected := `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -23,18 +23,18 @@ func TestGenDiff_FlatJSON(t *testing.T) {
   + timeout: 20
   + verbose: true
 }`
-	assert.Equal(t, expected, result)
+    assert.Equal(t, expected, result)
 }
 
 func TestGenDiff_FlatYAML(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1.yaml",
-		"testdata/fixture/file2.yaml",
-		"stylish",
-	)
-	require.NoError(t, err)
+    result, err := GenDiff(
+        "testdata/fixture/file1.yaml",
+        "testdata/fixture/file2.yaml",
+        "stylish",
+    )
+    require.NoError(t, err)
 
-	expected := `{
+    expected := `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -42,79 +42,145 @@ func TestGenDiff_FlatYAML(t *testing.T) {
   + timeout: 20
   + verbose: true
 }`
-	assert.Equal(t, expected, result)
+    assert.Equal(t, expected, result)
 }
 
 func TestGenDiff_NestedJSON_Stylish(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1_nested.json",
-		"testdata/fixture/file2_nested.json",
-		"stylish",
-	)
-	require.NoError(t, err)
+    result, err := GenDiff(
+        "testdata/fixture/file1_nested.json",
+        "testdata/fixture/file2_nested.json",
+        "stylish",
+    )
+    require.NoError(t, err)
 
-	assert.Contains(t, result, "common")
-	assert.Contains(t, result, "group1")
-	assert.Contains(t, result, "+ follow: false")
-	assert.Contains(t, result, "- setting2: 200")
-	assert.Contains(t, result, "setting1: Value 1")
+    expected := `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`
+    assert.Equal(t, expected, result)
 }
 
 func TestGenDiff_NestedJSON_Plain(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1_nested.json",
-		"testdata/fixture/file2_nested.json",
-		"plain",
-	)
-	require.NoError(t, err)
+    result, err := GenDiff(
+        "testdata/fixture/file1_nested.json",
+        "testdata/fixture/file2_nested.json",
+        "plain",
+    )
+    require.NoError(t, err)
 
-	assert.Contains(t, result, "Property 'common.follow' was added with value: false")
-	assert.Contains(t, result, "Property 'common.setting2' was removed")
-	assert.Contains(t, result, "Property 'common.setting3' was updated. From true to null")
+    expected := `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`
+    assert.Equal(t, expected, result)
 }
 
 func TestGenDiff_NestedJSON_JSON(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1_nested.json",
-		"testdata/fixture/file2_nested.json",
-		"json",
-	)
-	require.NoError(t, err)
+    result, err := GenDiff(
+        "testdata/fixture/file1_nested.json",
+        "testdata/fixture/file2_nested.json",
+        "json",
+    )
+    require.NoError(t, err)
 
-	assert.Contains(t, result, "{")
-	assert.Contains(t, result, "}")
-	assert.Contains(t, result, "common")
-	assert.Contains(t, result, "group1")
+    assert.Contains(t, result, "common")
+    assert.Contains(t, result, "group1")
+    assert.Contains(t, result, "group2")
+    assert.Contains(t, result, "group3")
+    assert.Contains(t, result, "follow")
+    assert.Contains(t, result, "setting6")
 }
 
-func TestGenDiff_NestedYAML(t *testing.T) {
-	result, err := GenDiff(
-		"testdata/fixture/file1_nested.yaml",
-		"testdata/fixture/file2_nested.yaml",
-		"stylish",
-	)
-	require.NoError(t, err)
+func TestGenDiff_NestedYAML_Stylish(t *testing.T) {
+    result, err := GenDiff(
+        "testdata/fixture/file1_nested.yaml",
+        "testdata/fixture/file2_nested.yaml",
+        "stylish",
+    )
+    require.NoError(t, err)
 
-	assert.Contains(t, result, "common")
-	assert.Contains(t, result, "group1")
-	assert.Contains(t, result, "+ follow: false")
+    assert.Contains(t, result, "common")
+    assert.Contains(t, result, "group1")
+    assert.Contains(t, result, "+ follow: false")
+    assert.Contains(t, result, "- setting2: 200")
+    assert.Contains(t, result, "doge")
+}
+
+func TestGenDiff_NestedYAML_Plain(t *testing.T) {
+    result, err := GenDiff(
+        "testdata/fixture/file1_nested.yaml",
+        "testdata/fixture/file2_nested.yaml",
+        "plain",
+    )
+    require.NoError(t, err)
+
+    assert.Contains(t, result, "Property 'common.follow' was added with value: false")
+    assert.Contains(t, result, "Property 'common.setting2' was removed")
+    assert.Contains(t, result, "Property 'common.setting6.doge.wow' was updated")
 }
 
 func TestGenDiff_InvalidFormat(t *testing.T) {
-	_, err := GenDiff(
-		"testdata/fixture/file1.json",
-		"testdata/fixture/file2.json",
-		"invalid",
-	)
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "unknown format")
+    _, err := GenDiff(
+        "testdata/fixture/file1.json",
+        "testdata/fixture/file2.json",
+        "invalid",
+    )
+    assert.Error(t, err)
+    assert.Contains(t, err.Error(), "unknown format")
 }
 
 func TestGenDiff_NonexistentFile(t *testing.T) {
-	_, err := GenDiff(
-		"nonexistent.json",
-		"testdata/fixture/file2.json",
-		"stylish",
-	)
-	assert.Error(t, err)
+    _, err := GenDiff(
+        "nonexistent.json",
+        "testdata/fixture/file2.json",
+        "stylish",
+    )
+    assert.Error(t, err)
 }
